81482f21913aa787e1193c5e1ab76822cab7b9eb
momen shalpy <momen.ahmed_2010@yahoo.com>
2023-03-06 02:25:09 +0200

    store controller (update - delete)

--- controller/storeController.js
+++ controller/storeController.js
@@ -9,7 +9,7 @@ exports.getStoresList = async (req, res) => {
 
         var STORE_LIST_QUERY = queries.quriesList.GET_STORE_LIST_QUERY;
         var result = await dbConnection.dbQuery(STORE_LIST_QUERY);
-        return res.status(200).send(JSON.stringify(result))
+        return res.status(200).send(JSON.stringify(result.rows))
 
 
     } catch (error) {
@@ -33,35 +33,114 @@ exports.createStore = async (req, res) => {
 
         var CREATED_ON = new Date()
 
-        if (!STORE_NAME || !STORE_ADDRESS) {
-            return res.status(500).send({ error: 'Name and Address are required.' })
+        if (!STORE_NAME) {
+            return res.status(500).send({ error: 'Name is required.' })
+        }
+        else if (!STORE_ADDRESS) {
+            return res.status(500).send({ error: 'Address is required.' })
+
         }
 
         var values = [STORE_NAME, STORE_CODE, STORE_ADDRESS, CREATED_ON, CREATED_BY]
 
         var CREATE_STORE_QUERY = queries.quriesList.CREATE_STORE_QUERY;
         console.log(CREATE_STORE_QUERY)
-        var result = await dbConnection.dbQuery(CREATE_STORE_QUERY,values)
+        var result = await dbConnection.dbQuery(CREATE_STORE_QUERY, values)
 
         console.log(result)
 
-        return res.status(201).send(JSON.stringify(result))
+        return res.status(201).send(STORE_NAME + " store is created successfully")
 
     }
     catch (error) {
-        return res.status(500).send({ error: 'failed to list store' })
+        return res.status(500).send({ error: 'Failed to create store' })
     }
 }
 
 
-exports.updateStore = (req, res) => {
+exports.updateStore = async (req, res) => {
+    try {
+
+        var STORE_NAME = req.body.STORE_NAME
+
+        var STORE_ADDRESS = req.body.STORE_ADDRESS
+
+        var STORE_ID = req.body.STORE_ID
+
+        
+            var DB_STORE_ID_query = queries.quriesList.GET_STORE_ID_QUERY
+
+            var DB_STORE_ID = await dbConnection.dbQuery(DB_STORE_ID_query, [STORE_ID])
+
+            console.log(DB_STORE_ID.rows[0].count)
+
+            // if (!DB_STORE_ID) {
+            //     return res.status(500).send(STORE_ID + " ID Not Found")
+
+            // }
+        
+           
+        
+
+
+        var CREATED_BY = "admin"
+
+        var CREATED_ON = new Date()
+
+        if (!STORE_NAME) {
+            return res.status(500).send({ error: 'Name is required.' })
+        }
+        else if (!STORE_ADDRESS) {
+            return res.status(500).send({ error: 'Address is required.' })
+
+        }
+        if (DB_STORE_ID.rows[0].count == "0") {
+            return res.status(500).send({ error: 'ID Doesnt exist' })
+        }
+        var values = [STORE_NAME, STORE_ADDRESS, CREATED_ON, CREATED_BY, STORE_ID]
+
+
+
+        var UPDATE_STORE_QUERY = queries.quriesList.UPDATE_STORE_QUERY;
+
+        var result = await dbConnection.dbQuery(UPDATE_STORE_QUERY, values)
+
+        return res.status(201).send(" store is updated successfully")
+
+    }
+    catch (error) {
+        return res.status(500).send({ error: 'Failed to Update store' })
+    }
 
 }
 
 
-exports.deleteStore = (req, res) => {
+exports.deleteStore = async  (req, res) => {
 
-    return res.status(202).send("Note Deleted")
+
+    try {
+        var STORE_ID = req.body.STORE_ID
+
+        var DB_STORE_ID_query = queries.quriesList.GET_STORE_ID_QUERY
+
+        var DB_STORE_ID = await dbConnection.dbQuery(DB_STORE_ID_query, [STORE_ID])
+
+
+        if (DB_STORE_ID.rows[0].count == "0") {
+            return res.status(500).send({ error: 'Store Doesnt exist' })
+        }
+
+        var delete_query = queries.quriesList.DELETE_STORE_QUERE;
+
+        var result = await dbConnection.dbQuery(delete_query, [STORE_ID])
+
+        return res.status(200).send("Store Deleted successfully")
+
+    }
+    catch (error) {
+        return res.status(500).send({ error: 'Failed to Delete store' })
+    }
+
 
 }
 
--- db/queries.js
+++ db/queries.js
@@ -1,6 +1,17 @@
 exports.quriesList ={
     GET_STORE_LIST_QUERY : 'SELECT store_id, store_name, store_code, store_address, created_on, created_by FROM bms.store',
-    CREATE_STORE_QUERY : 'INSERT INTO bms.store (store_name, store_code, store_address, created_on, created_by) VALUES($1, $2,$3, $4,$5)'
+
+    CREATE_STORE_QUERY : 'INSERT INTO bms.store (store_name, store_code, store_address, created_on, created_by) VALUES($1, $2,$3, $4,$5)',
+
+    UPDATE_STORE_QUERY : "UPDATE bms.store SET store_name=$1, store_address=$2, created_on=$3, created_by=$4 WHERE store_id=$5",
+
+    GET_STORE_ID_QUERY:'SELECT COUNT(store_id) FROM bms.store WHERE store_id = $1;',
+
+    DELETE_STORE_QUERE:'DELETE FROM bms.store WHERE store_id=$1;'
+
+
+
+
 }
 
 // exports.queries = {
